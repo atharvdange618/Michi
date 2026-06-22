6e94351edfe43a94f75e4debc60157a405970b96
Atharv Dange <atharvdange618@gmail.com>
2026-06-23 00:59:13 +0530

    chore(demo): add ngrok allowed host to vite config

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["splittable-jeane-unsplit.ngrok-free.dev"],
+  },
 })
